09b076b0f067536c1f9abea6d7613af457ad58f5
Hassan-Anwar <hassananwar79@gmail.com>
2021-02-02 20:53:07 +0000

    Updated return method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -43,11 +43,11 @@ namespace GradeBook.GradeBooks
             if(Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculatea student's overall grade.");
+                return;
             }
-            else
-            {
+         
                 base.CalculateStatistics();
-            }
+      
         }
 
         public override void CalculateStudentStatistics(string name)
@@ -55,6 +55,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
 
             base.CalculateStudentStatistics(name);
